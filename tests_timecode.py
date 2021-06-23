dd977efc5c2471f3c43c7c64b84f3d72d1694b47
Michael Jordan <michael@glowingpixel.com>
2021-06-22 19:51:50 -0700

    All-inclusive math and comparison (#4)
    * Drop frames are killing me plz help
    
    * Comparisons and math fixes

--- tests_timecode.py
+++ tests_timecode.py
@@ -1,6 +1,6 @@
 from datetime import time
 import unittest
-from posttools.timecode import Timecode, TimecodeRange
+from posttools.timecode import IncompatibleTimecode, InvalidTimecode, Timecode, TimecodeRange
 
 class TestInstantiation(unittest.TestCase):
 	
@@ -12,21 +12,27 @@ class TestInstantiation(unittest.TestCase):
 		self.assertEqual(str(tc), "01:00:00:00")
 		self.assertEqual(tc.mode, Timecode.Mode.NDF)
 	
-	def test_30_fromstring(self):
+	def test_30NDF_fromstring(self):
 		tc = Timecode("01:00:00:00", 30)
 		self.assertIsInstance(tc, Timecode)
 		self.assertEqual(tc.rate,30)
 		self.assertEqual(str(tc), "01:00:00:00")
 		self.assertEqual(tc.framenumber, 108000)
 	
+	def test_30NDF_late_fromstring(self):
+		tc = Timecode("22:15:08:13", 30)
+		self.assertIsInstance(tc, Timecode)
+		self.assertEqual(tc.rate,30)
+		self.assertEqual(str(tc), "22:15:08:13")
+	"""	
 	def test_30DF_fromstring(self):
 		tc = Timecode("01:00:00:00", 30, Timecode.Mode.DF)
 		self.assertEqual(str(tc), "01:00:00;00")
 
-	def test_30DF_fromstring(self):
+	def test_30DF_late_fromstring(self):
 		tc = Timecode("20:00:05;00", 30, Timecode.Mode.DF)
 		self.assertEqual(str(tc), "20:00:05;00")
-
+	"""
 	def test_24_fromint(self):
 		tc = Timecode(86400)
 		self.assertIsInstance(tc, Timecode)
@@ -80,6 +86,73 @@ class TestTimecodeConversions(unittest.TestCase):
 
 	def test_30DF_to_30DF(self):
 		pass
+
+class TestTimecodeMath(unittest.TestCase):
+
+	def test_add_number(self):
+		self.assertEqual(Timecode("01:00:00:00") + 2, Timecode("01:00:00:02"))
+		self.assertEqual(Timecode("23:12:14:22") - 20, Timecode("23:12:14:02"))
+		self.assertEqual(Timecode("16:42:13:11", 30) + 31, Timecode("16:42:14:12", 30))
+	
+	def test_add_string(self):
+		self.assertEqual(Timecode("01:00:00:00") + "02:25:16", Timecode("01:02:25:16"))
+		self.assertEqual(Timecode("20:18:12:10", 30) + "1:1:1:1", Timecode("21:19:13:11", 30))
+		self.assertEqual(Timecode("18:15", 15) - ":15", Timecode("00:00:18:00", 15))
+		self.assertRaises(InvalidTimecode, lambda: Timecode("01:00:00:00") + "poop")
+	
+	def test_add_Timecode(self):
+		self.assertEqual(Timecode("12:00:00:00") + Timecode("12:00:00:00"), Timecode("24:00:00:00"))
+		self.assertEqual(Timecode("12:12:15:12") + Timecode("12:15:15",30), Timecode("12:24:31:00"))
+		self.assertEqual(Timecode("12:12:15:15",30) + Timecode("12:15:12",24), Timecode("12:24:31:00",30))
+	
+	def test_equality(self):
+		self.assertTrue(Timecode("12:12:12:12") == Timecode("12:12:12:12"))
+		self.assertFalse(Timecode("12:12:12:12") == Timecode("12:12:12:13"))
+		self.assertTrue(Timecode("12:12:12:12") != Timecode("12:12:12:12", 30))
+		self.assertTrue(Timecode("12:12:12:12", 30) != Timecode("12:12:12:12", rate=30, mode=Timecode.Mode.DF))
+		self.assertTrue(Timecode("01:00:00;00", rate=30, mode=Timecode.Mode.DF) == Timecode("01:00:00;00", rate=30, mode=Timecode.Mode.DF))
+		self.assertTrue(Timecode("01:00:00:00") == 86400)
+		self.assertTrue(Timecode("01:00:00;00", rate=30, mode=Timecode.Mode.DF) == 108000)	# TODO: Based on bad DF string to frame math. Will fail in future, and frankly, should.
+	
+	def test_cmp(self):
+		self.assertTrue(Timecode("12:12:12:12") < Timecode("12:12:12:13"))
+		self.assertTrue(Timecode("12:12:12:12") <= Timecode("12:12:12:13"))
+		self.assertFalse(Timecode("12:12:12:12") > Timecode("12:12:12:13"))
+		self.assertFalse(Timecode("12:12:12:12") >= Timecode("12:12:12:13"))
+
+		self.assertTrue(Timecode("12:12:12:12") < Timecode("12:12:12:12", rate=30))
+		self.assertTrue(Timecode("12:12:12:12") < Timecode("12:12:12:12", rate=30, mode=Timecode.Mode.DF))
+		self.assertTrue(Timecode("12:12:12:12", rate=30) < Timecode("12:12:12:12", rate=30, mode=Timecode.Mode.DF))
+	
+	def test_sort(self):
+
+		timecodes = [
+			Timecode("12:00:00:00"),
+			Timecode("11:00:00:13"), 
+			Timecode("13:18:22:11", rate=30),
+			Timecode("17:32:14;11", rate=30, mode=Timecode.Mode.DF),
+			Timecode("314:31:24:14"),
+			Timecode("13:18:22:11", rate=30),
+			Timecode("17:32:14;11", rate=30, mode=Timecode.Mode.DF),
+			Timecode("24:13:11:02", rate=15),
+			Timecode("01:00:00;00", rate=30),
+			Timecode("01:00:00:00", rate=30, mode=Timecode.Mode.DF)
+		]
+
+		timecodes_sorted = [
+			Timecode("24:13:11:02", rate=15),
+			Timecode("11:00:00:13"), 
+			Timecode("12:00:00:00"),
+			Timecode("314:31:24:14"),
+			Timecode("01:00:00;00", rate=30),
+			Timecode("13:18:22:11", rate=30),
+			Timecode("13:18:22:11", rate=30),
+			Timecode("01:00:00:00", rate=30, mode=Timecode.Mode.DF),
+			Timecode("17:32:14;11", rate=30, mode=Timecode.Mode.DF),
+			Timecode("17:32:14;11", rate=30, mode=Timecode.Mode.DF)
+		]
+		self.assertEqual(sorted(timecodes), timecodes_sorted)
+		self.assertEqual(len(set(timecodes)), 8)
 
 class TestTimecodeFormatting(unittest.TestCase):
 
