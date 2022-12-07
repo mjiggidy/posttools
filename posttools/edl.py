282399a8ddcece57ad0670006a3845a8bb4120f4
Michael Jordan <michael@glowingpixel.com>
2022-12-06 16:50:00 -0800

    Notes

--- posttools/edl.py
+++ posttools/edl.py
@@ -3,16 +3,71 @@ from posttools.timecode import Timecode, TimecodeRange
 
 class Event:
 	"""An EDL Event"""
+	# CMX3600: Standard Form Statement
 	pat_event = re.compile(r"^(?P<event_number>\d+)\s+(?P<reel_name>[^\s]+)\s+(?P<track_type>A\d*|B|V)\s+(?P<event_type>C|D|W\d+|K\s*[BO]?)\s+(?P<event_duration>\d*)\s+(?P<tc_src_in>\d{2}:\d{2}:\d{2}:\d{2})\s+(?P<tc_src_out>\d{2}:\d{2}:\d{2}:\d{2})\s+(?P<tc_rec_in>\d{2}:\d{2}:\d{2}:\d{2})\s+(?P<tc_rec_out>\d{2}:\d{2}:\d{2}:\d{2})\s*$", re.I)
+
+	"""
+	TODO: CMX3600:
+	F1: Event number. Decimal only.  If not decimal, considered note or note statement
+	F2: Source. Three-digit reel number (001-253) followed by optional B.  Or BL (Black) or AX (Aux)
+	F3: Channels involved A (Audio 1) B (Audio 1 and Video) V (Video) A2 (Audio 2) A2/V (Audio 2 and video) AA (Audio 1 and Audio 2) AA/V (A1/2 & Video)
+	F4: Type of edit statement C (cut) D(dissolve), Wxxx (Wipe+CMX wipe code), KB (is the backround of a key), K (is key foreground), KO (is keyed out of the foreground)
+	F5: Spaces if C.  001-255 if W, D, or K(?), "(F)" if KB with a fade condition
+	F6: Source In
+	F7: Source Out
+	F8: Record In
+	F9: Record Out (Reference only: CMX systems calculate this based on F6-8)
+
+	"""
 
 	def __init__(self, reel:str, track:"Track", source_tc:TimecodeRange, record_tc:TimecodeRange):
 		self._track = track
-		self.reel = reel
+		self._reel = reel
+		self._tc_source = source_tc
+		self._tc_record = record_tc
+		self.notes = list() # TODO: Temp thing
+
+		# TODO: CMX3600: BL (black) AX (aux) sources
 		
 	@property
 	def track(self) -> "Track":
 		"""The track this event belongs to"""
 		return self._track
+	
+	@property
+	def reel(self) -> str:
+		return self._reel
+	
+	@property
+	def tc_source(self) -> TimecodeRange:
+		return self._tc_source
+	
+	@property
+	def tc_record(self) -> TimecodeRange:
+		return self._tc_record
+	
+	def __str__(self) -> str:
+		notes = "\n".join(self.notes)
+		return f"{self.reel} ({self.tc_record}):\n{notes}\n"
+
+class Note:
+	"""Note form statement"""
+
+	""""
+	TODO: CMX3600:
+	FCM:	Frame code mode change (goes before event)
+	SPLIT:	Audio/Video split in-time (goes before event) (?)
+	GPI		GPI trigger
+	M/S		Master/Slave
+	SWM		Switcher memory
+	M2		Motion memory
+	%		Motion memory variable data
+
+	Split examples: (TC is the delay relative to the in-point)
+		SPLIT:    AUDIO DELAY=  00:00:00:05
+		SPLIT:    VIDEO DELAY=  00:00:02:00
+	"""
+	
 
 class Track:
 	"""A track containing events in the EDL"""
@@ -25,9 +80,6 @@ class Track:
 
 		AUDIO = "A"
 		"""Audio track"""
-
-		BLACK = "B"
-		"""Black"""
 
 	def __init__(self, name:str):
 		"""Create a new EDL track"""
@@ -66,6 +118,7 @@ class Track:
 
 class Fcm(enum.Enum):
 	"""EDL frame counting modes"""
+	# TODO: CMX3600: FCM does not appear in PAL systems
 	NON_DROP_FRAME = "NON-DROP FRAME"
 	DROP_FRAME = "DROP FRAME"
 
@@ -81,8 +134,8 @@ class Edl:
 	
 	def __init__(self, *, title:str="Untitled EDL", fcm:Fcm=Fcm.NON_DROP_FRAME, events:typing.Optional[typing.Iterable[Event]]=None):
 
-		self._title = title
-		self._fcm   = fcm
+		self.title = title
+		self.fcm   = fcm
 		self._events = list(events) if events else []
 
 	@classmethod
@@ -92,8 +145,8 @@ class Edl:
 		mode =_ParseModes.TITLE
 		events = list()
 
+		buffer = []
 		for line_num, line_edl in enumerate(l.rstrip('\n') for l in file.readlines()):
-
 
 			try:
 				if mode == _ParseModes.TITLE:
@@ -111,8 +164,11 @@ class Edl:
 					event_match = Event.pat_event.match(line_edl)
 					if event_match:
 						event = cls._parse_new_event(event_match)
+						event.notes.extend(buffer)
+						buffer=[]
 						events.append(event)
-
+					else:
+						buffer.append(line_edl)
 				
 			except Exception as e:
 				raise ValueError(f"Line {line_num+1}: {e}")
@@ -151,7 +207,7 @@ class Edl:
 		source_tc = TimecodeRange(start=Timecode(event_match.group("tc_src_in")), end=Timecode(event_match.group("tc_src_out")))
 		record_tc = TimecodeRange(start=Timecode(event_match.group("tc_rec_in")), end=Timecode(event_match.group("tc_rec_out")))
 
-		print(source_tc, record_tc)
+		#print(source_tc, record_tc)
 
 
 		return Event(reel=event_match.group("reel_name"), source_tc=source_tc, record_tc=record_tc, track=track)
@@ -162,12 +218,13 @@ class Edl:
 		print(f"TITLE: {self.title}", file=file)
 		print(f"FCM: {self.fcm.value}", file=file)
 
-#		for event in self.events:
-#			print(event, file=file)
+		for event in self.events:
+			print(event, file=file)
 
 	@property
 	def title(self) -> str:
 		"""The title of the EDL"""
+		#CMX3600: <=70 chars. Uppercase letters, spaces, numbers only
 		return self._title
 	
 	@title.setter
@@ -191,7 +248,7 @@ class Edl:
 	
 	@fcm.setter
 	def fcm(self, fcm:Fcm):
-		if not isinstance(fcm, self.Fcm):
+		if not isinstance(fcm, Fcm):
 			raise ValueError("Invalid FCM provided")
 		self._fcm = fcm
 	
