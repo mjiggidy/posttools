add1317e4a976a4b58b92557b6835d20a3671089
Michael Jordan <michael@glowingpixel.com>
2023-04-04 17:05:24 -0700

    Progress(?) with standard form statements

--- posttools/edl.py
+++ posttools/edl.py
@@ -1,4 +1,4 @@
-import enum, typing, io, re, functools
+import enum, typing, io, re, functools, abc
 from posttools.timecode import Timecode, TimecodeRange
 
 class Fcm(enum.Enum):
@@ -14,10 +14,77 @@ class _ParseModes(enum.Enum):
 	EVENT_START = enum.auto()
 	EVENT_DETAILS = enum.auto()
 
-class Event:
-	"""An EDL Event"""
-	# CMX3600: Standard Form Statement
-	pat_event = re.compile(r"^(?P<event_number>\d+)\s+(?P<reel_name>[^\s]+)\s+(?P<track_type>A\d*|B|V)\s+(?P<event_type>C|D|W\d+|K\s*[BO]?)\s+(?P<event_duration>\d*)\s+(?P<tc_src_in>\d{2}:\d{2}:\d{2}:\d{2})\s+(?P<tc_src_out>\d{2}:\d{2}:\d{2}:\d{2})\s+(?P<tc_rec_in>\d{2}:\d{2}:\d{2}:\d{2})\s+(?P<tc_rec_out>\d{2}:\d{2}:\d{2}:\d{2})\s*$", re.I)
+class SourceReel(abc.ABC):
+	"""A base source for use as a reel name in a Standard Form Statement"""
+
+	@property
+	def _NAME(self) -> str:
+		pass
+
+	@property
+	def name(self) -> str:
+		"""The reel name"""
+		return self._NAME
+	
+	def __str__(self):
+		return self.name
+	
+	def __eq__(self, other):
+		return self._NAME == other._NAME
+	
+	def __hash__(self):
+		return hash(self._NAME)
+	
+	@classmethod
+	def from_string(cls, reel_name:str):
+
+		if BlackSource.validate(reel_name):
+			return BlackSource
+		elif AuxSource.validate(reel_name):
+			return AuxSource
+		elif TapeSource.validate(reel_name):
+			return TapeSource(reel_name)
+		else:
+			raise ValueError("Reel name format is not recognized")
+
+class BlackSource(SourceReel):
+	"""A black/empty source"""
+
+	_NAME = "BL"
+
+	@classmethod
+	def validate(self, reel_name:str) -> bool:
+		"""Is this a valid Black source"""
+		return reel_name.upper() == "BL"
+
+class AuxSource(SourceReel):
+	"""An auxillary source"""
+
+	_NAME = "AX"
+
+	@classmethod
+	def validate(self, reel_name:str) -> bool:
+		return reel_name.upper() == "AX"
+
+class TapeSource(SourceReel):
+
+	_NAME = str()
+
+	def __init__(self, reel_name:str):
+
+		if not self.validate(reel_name):
+			raise ValueError(f"Reel name contains invalid characters for this source type")
+
+		super().__init__()
+		self._NAME = reel_name
+
+	@classmethod
+	def validate(cls, reel_name:str) -> bool:
+		return len(reel_name.strip()) and not re.search("\s", reel_name)
+
+
+class StandardFormStatement(abc.ABC):
+	"""A base standard form statement"""
 
 	"""
 	TODO: CMX3600:
@@ -33,41 +100,345 @@ class Event:
 
 	"""
 
-	def __init__(self, reel:str, track:"Track", source_tc:TimecodeRange, record_tc:TimecodeRange, fcm:Fcm):
-		self._track = track
-		self._fcm = fcm
-		self._reel = reel
-		self._tc_source = source_tc
-		self._tc_record = record_tc
-		self.notes = list() # TODO: Temp thing
+	def __init__(self, reel_name:str, tracks:typing.Iterable["Track"], timecode_source:TimecodeRange, timecode_record:TimecodeRange, event_number:typing.Optional[int]=None):
+		"""Basic parsing of common elements"""
+		self._reel = SourceReel.from_string(reel_name)
+		self._tracks = tracks
+		self._timecode_source = timecode_source
+		self._timecode_record = timecode_record
+		self._event_number = int(event_number) if event_number is not None else None
 
-		# TODO: CMX3600: BL (black) AX (aux) sources
+	@classmethod
+	def all_statement_types(cls) -> typing.Generator["StandardFormStatement", None, None]:
+		"""Return all subclasses of this type of statement"""
+
+		for statement in cls.__subclasses__():
+			yield from statement.all_statement_types()
+			yield statement
+	
+	@property
+	def PAT_EVENT(self) -> re.Pattern:
+		"""Regex pattern matching this statement type"""
+		pass
+
+	@abc.abstractclassmethod
+	def parse_from_pattern(self, statement:re.Pattern) -> "StandardFormStatement":
+		"""Create a statement object from a parsed regex object"""
+		pass
+
+	@classmethod
+	def parse_from_string(cls, line:str) -> "StandardFormStatement":
+		"""Create a statement object from a given line from an EDL"""
+		pat = cls.PAT_EVENT.match(line)
+		if not pat:
+			raise ValueError(f"This line is not a valid {cls.__name__}")
+		return cls.parse_from_pattern(pat)
+	
+	@classmethod
+	def _parse_shared_elements(cls, statement:re.Pattern) -> typing.Tuple[int, str, typing.Set["Track"], TimecodeRange, TimecodeRange]:
+
+		event_number = int(statement.group("event_number"))
+		reel_name = statement.group("reel_name")
+		tracks = {Track(statement.group("track_type"))}
+		timecode_source = TimecodeRange(
+			start=Timecode(statement.group("tc_src_in")), end=Timecode(statement.group("tc_src_out"))
+		)
+		timecode_record = TimecodeRange(
+			start=Timecode(statement.group("tc_rec_in")), end=Timecode(statement.group("tc_rec_out"))
+		)
+
+		return event_number, reel_name, tracks, timecode_source, timecode_record
+	
+	@property
+	def source(self) -> SourceReel:
+		"""The source reel referenced for this statement"""
+		return self._reel
+
+	@property
+	def reel_name(self) -> str:
+		"""The reel name referenced for this statement"""
+		return str(self.source._NAME)
+	
+	@property
+	def tracks(self) -> typing.Set["Track"]:
+		"""The tracks referenced in this statement"""
+		return self._tracks
+	
+	@property
+	def timecode_source(self) -> TimecodeRange:
+		"""The timecode range used by the source reel"""
+		return self._timecode_source
+	
+	@property
+	def timecode_record(self) -> TimecodeRange:
+		"""The timecode range the source appears in the timeline"""
+		return self._timecode_record
+	
+	@property
+	def event_number(self) -> typing.Union[int,None]:
+		"""The original event number from the originating EDL"""
+		return self._event_number
+	
+	@property
+	def fcm(self) -> "Fcm":
+		"""The Frame Counting Mode for this statement"""
+		# TODO: No hardcode no mo plz bb
+		return Fcm.NON_DROP_FRAME
+
+class CutStatement(StandardFormStatement):
+	"""A simple cut"""
+
+	def __init__(self, reel_name:str, tracks:typing.Iterable["Track"], timecode_source:TimecodeRange, timecode_record:TimecodeRange, event_number:typing.Optional[int]=None):
+
+		super().__init__(
+			reel_name=reel_name,
+			timecode_source=timecode_source,
+			timecode_record=timecode_record,
+			tracks=tracks,
+			event_number=event_number
+		)
+
+	PAT_EVENT = re.compile(
+		r"^(?P<event_number>\d+)\s+"
+		r"(?P<reel_name>[^\s]+)\s+"
+		r"(?P<track_type>A\d*|B|V)\s+"
+		r"(?P<event_type>C)\s+"
+		r"(?P<tc_src_in>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_src_out>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_rec_in>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_rec_out>\d{2}:\d{2}:\d{2}:\d{2})\s*$"
+	, re.I)
+
+	@classmethod
+	def parse_from_pattern(cls, statement:re.Pattern) -> "CutStatement":
+		"""Create a Cut Statement from a parsed regex string"""
+
+		event_number, reel_name, tracks, timecode_source, timecode_record = super()._parse_shared_elements(statement)
+
+		return cls(
+			reel_name = reel_name,
+			tracks = tracks,
+			timecode_source = timecode_source,
+			timecode_record = timecode_record,
+			event_number = event_number
+		)
+	
+	def __str__(self):
+		# TODO: Additional formatting options (spacing, number padding)
+		return f"{str(self.event_number if self.event_number is not None else 1).zfill(3)}  {self.reel_name.ljust(128)}  {str().join(t.name for t in self.tracks).ljust(3)}  C       {self.timecode_source.start} {self.timecode_source.end} {self.timecode_record.start} {self.timecode_record.end}"
+
+class DissolveStatement(StandardFormStatement):
+	"""A dissolve statement"""
+
+	PAT_EVENT = re.compile(
+		r"^(?P<event_number>\d+)\s+"
+		r"(?P<reel_name>[^\s]+)\s+"
+		r"(?P<track_type>A\d*|B|V)\s+"
+		r"(?P<event_type>D)\s+"
+		r"(?P<event_duration>\d+)\s+"
+		r"(?P<tc_src_in>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_src_out>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_rec_in>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_rec_out>\d{2}:\d{2}:\d{2}:\d{2})\s*$"
+	, re.I)
+
+	def __init__(self, reel_name:str, tracks:typing.Iterable["Track"], timecode_source:TimecodeRange, timecode_record:TimecodeRange, dissolve_length:int, event_number:typing.Optional[int]=None):
+
+		super().__init__(
+			reel_name=reel_name,
+			timecode_source=timecode_source,
+			timecode_record=timecode_record,
+			tracks=tracks,
+			event_number=event_number
+		)
+
+		self._dissolve_length = int(dissolve_length)
+	
+	@property
+	def dissolve_length(self) -> int:
+		"""Length in frames of the dissolve"""
+		return self._dissolve_length
+
+	@classmethod
+	def parse_from_pattern(cls, statement:re.Pattern) -> "DissolveStatement":
+		"""Create a Dissolve Statement from a parsed regex string"""
+
+		event_number, reel_name, tracks, timecode_source, timecode_record = super()._parse_shared_elements(statement)
+
+		return cls(
+			reel_name = reel_name,
+			tracks = tracks,
+			dissolve_length = int(statement.group("event_duration")),
+			timecode_source = timecode_source,
+			timecode_record = timecode_record,
+			event_number = event_number
+		)
+
+	def __str__(self):
+		# TODO: Additional formatting options
+		return f"{str(self.event_number if self.event_number is not None else 1).zfill(3)}  {self.reel_name.ljust(128)}  {str().join(t.name for t in self.tracks).ljust(3)}  D  {str(self.dissolve_length).zfill(3)}  {self.timecode_source.start} {self.timecode_source.end} {self.timecode_record.start} {self.timecode_record.end}"
+
+class WipeStatement(StandardFormStatement):
+	"""A wipe statement"""
+
+	PAT_EVENT = re.compile(
+		r"^(?P<event_number>\d+)\s+"
+		r"(?P<reel_name>[^\s]+)\s+"
+		r"(?P<track_type>A\d*|B|V)\s+"
+		r"(?P<event_type>W\d+)\s+"
+		r"(?P<event_duration>\d+)\s+"
+		r"(?P<tc_src_in>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_src_out>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_rec_in>\d{2}:\d{2}:\d{2}:\d{2})\s+"
+		r"(?P<tc_rec_out>\d{2}:\d{2}:\d{2}:\d{2})\s*$"
+	, re.I)
+
+	def __init__(self, reel_name:str, tracks:typing.Iterable["Track"], timecode_source:TimecodeRange, timecode_record:TimecodeRange, wipe_length:int, wipe_id:int, event_number:typing.Optional[int]=None):
+
+		super().__init__(
+			reel_name=reel_name,
+			timecode_source=timecode_source,
+			timecode_record=timecode_record,
+			tracks=tracks,
+			event_number=event_number
+		)
+
+		self._wipe_length = int(wipe_length)
+		self._wipe_id     = int(wipe_id)
+
+	@classmethod
+	def parse_from_pattern(cls, statement:re.Pattern) -> "WipeStatement":
+		"""Create a Dissolve Statement from a parsed regex string"""
+
+		event_number, reel_name, tracks, timecode_source, timecode_record = super()._parse_shared_elements(statement)
+
+		return cls(
+			reel_name = reel_name,
+			tracks = tracks,
+			wipe_length = int(statement.group("event_duration")),
+			wipe_id     = int(statement.group("event_type")[1:]),
+			timecode_source = timecode_source,
+			timecode_record = timecode_record,
+			event_number = event_number
+		)
+	
+	@property
+	def wipe_length(self) -> int:
+		"""Length in frames of the wipe"""
+		return self._wipe_length
+	
+	@property
+	def wipe_id(self) -> int:
+		"""CMX Wipe ID"""
+		return self._wipe_id
+	
+	def __str__(self):
+		# TODO: Additional formatting options
+		return f"{str(self.event_number if self.event_number is not None else 1).zfill(3)}  {self.reel_name.ljust(128)}  {str().join(t.name for t in self.tracks).ljust(3)}  W{str(self.wipe_id).zfill(3)}  {str(self.wipe_length).zfill(3)}  {self.timecode_source.start} {self.timecode_source.end} {self.timecode_record.start} {self.timecode_record.end}"
+
+class NoteFormStatement(abc.ABC):
+	pass
+
+class Event:
+	"""An EDL Event"""
+
+	def __init__(self, standard_statements:typing.Iterable[StandardFormStatement], note_statements:typing.Optional[typing.Iterable["NoteFormStatement"]]=None, comments=typing.Optional[typing.Iterable[str]]):
+
+		self._sfs = list(standard_statements)
+		self._nfs = note_statements if note_statements else []
+		
+		fcm = {s.fcm for s in self._sfs}
+		if len(fcm) != 1:
+			raise ValueError(f"Standard Form Statements must have matching FCMs")
+		self._fcm = fcm.pop()
+
+		self.comments = comments if comments else list() # TODO: Temp thing
+
+	@classmethod
+	def from_string(cls, event:str) -> "Event":
+		"""Parse an event from an event string"""
+
+		sfs = list()
+		nfs = list()
+		comments = list()
+
+		for line in event.splitlines(keepends=False):
+			if not line.strip():
+				continue
+
+			parsed = cls._identify_line(line)
+			if isinstance(parsed, StandardFormStatement):
+				sfs.append(parsed)
+			elif isinstance(parsed, str):
+				comments.append(parsed)
+			else:
+				raise ValueError(f"Unrecognized line (of type {type(parsed)})in event: {line}")
+		
+		return cls(
+			standard_statements = sfs,
+			note_statements = nfs,
+			comments = comments
+		)
+			
+	@classmethod
+	def _identify_line(cls, line:str) -> typing.Union[re.Pattern, str, None]:
+		"""Identify a line"""
+		for s in StandardFormStatement.all_statement_types():
+			match = s.PAT_EVENT.match(line)
+			if match:
+				parsed = s.parse_from_pattern(match)
+				return parsed
+		return line
 		
 	@property
-	def track(self) -> "Track":
-		"""The track this event belongs to"""
-		return self._track
+	def tracks(self) -> typing.Set["Track"]:
+		"""The track(s) this event belongs to"""
+		tracks = set()
+		for s in self._sfs:
+			for track in s.tracks:
+				tracks.add(track)
+		return tracks
 	
 	@property
-	def reel(self) -> str:
-		return self._reel
+	def sources(self) -> typing.Set[SourceReel]:
+		return set(s.source for s in self._sfs)
 	
 	@property
-	def tc_source(self) -> TimecodeRange:
-		return self._tc_source
+	def reel_names(self) -> typing.Set[str]:
+		return set(s.reel_name for s in self._sfs)
 	
 	@property
-	def tc_record(self) -> TimecodeRange:
-		return self._tc_record
-
+	def timecode_extents(self) -> TimecodeRange:
+		"""The full extents of this event"""
+		return TimecodeRange(
+			start = min(s.timecode_record.start for s in self._sfs),
+			end   = max(s.timecode_record.end for s in self._sfs)
+		)
+	
+	@property
+	def duration(self) -> Timecode:
+		"""The duraction of this event"""
+		return self.timecode_extents.duration
+	
 	@property
 	def fcm(self) -> Fcm:
 		"""Frame counting mode of this event"""
 		return self._fcm
 	
 	def __str__(self) -> str:
-		notes = "\n".join(self.notes)
-		return f"{self.reel} ({self.tc_record}):\n{notes}\n"
+		# TODO: Add the rest
+		output = str()
+		output += "\n".join(str(s) for s in self._sfs)
+
+		output += "\n"
+
+		output += "\n".join(str(n) for n in self._nfs)
+
+		output += "\n"
+
+		output += "\n".join(str(c) for c in self.comments)
+
+		return output
 
 class Note:
 	"""Note form statement"""
@@ -177,7 +548,7 @@ class Edl:
 			try:
 				# If starting next event, process event buffer and flush
 				if event_buffer and cls._is_begin_new_event(line_edl, current_index):
-					events.append(cls._parse_new_event_from_buffer(event_buffer))
+					events.append(Event.from_string("\n".join(event_buffer)))
 					event_buffer=[]
 					current_index = 0
 				
@@ -193,7 +564,7 @@ class Edl:
 		# Take care of the last little feller.
 		# TODO: How to not have to do this?
 		if event_buffer:
-			events.append(cls._parse_new_event_from_buffer(event_buffer))
+			events.append(Event.from_string("\n".join(event_buffer)))
 		
 		return cls(title=title, fcm=Fcm.NON_DROP_FRAME, events=events)
 	
@@ -207,7 +578,7 @@ class Edl:
 		first_token = line.split(maxsplit=1)[0]
 		
 		# Encountered prefixed form statement while parsing an event
-		if first_token.lower() in ["FCM:","SPLIT:"]:
+		if first_token.lower() in {"FCM:","SPLIT:"}:
 			return True
 		
 		# Encountered an event number different than the one we been doin'
@@ -219,10 +590,10 @@ class Edl:
 	@staticmethod
 	def _parse_title_from_line(line:str) -> str:
 		"""Extract a title from a line in an EDL"""
-		start = "title:"
-		if not line.lower().startswith(start):
+		START = "title:"
+		if not line.lower().startswith(START):
 			raise ValueError("Title was expected, but not found")
-		title = line[len(start):].strip()
+		title = line[len(START):].strip()
 		if not len(title):
 			raise ValueError("Title is empty")
 		return title
@@ -230,41 +601,14 @@ class Edl:
 	@staticmethod
 	def _parse_fcm_from_line(line:str) -> Fcm:
 		"""Extract the FCM from a line in an EDL"""
-		start = "fcm:"
-		if not line.lower().startswith(start):
+		START = "fcm:"
+		if not line.lower().startswith(START):
 			raise ValueError("FCM was expected, but not found")
 		try:
-			fcm = Fcm(line[len(start):].strip())
+			fcm = Fcm(line[len(START):].strip())
 		except:
 			raise ValueError("Invalid FCM specified")
 		return fcm
-	
-	# TODO: Maybe these parsers belong in the class they are related to.
-	# Maybe that makes TOO much sense.
-	@staticmethod
-	def _parse_new_event_from_buffer(buffer:typing.Iterable[str]) -> Event:
-		"""Create a new event based on that match"""
-
-		event = None
-		notes = []
-		for line in buffer:
-			event_match = Event.pat_event.match(line)
-			if event_match:
-				event = Event(
-					reel = event_match.group("reel_name"),
-					source_tc = TimecodeRange(start=Timecode(event_match.group("tc_src_in")), end=Timecode(event_match.group("tc_src_out"))),
-					record_tc = TimecodeRange(start=Timecode(event_match.group("tc_rec_in")), end=Timecode(event_match.group("tc_rec_out"))),
-					track = Track(event_match.group("track_type")),
-					fcm=Fcm.NON_DROP_FRAME #TODO: CHANGE THIS
-				)
-
-			else:
-				notes.append(line)
-
-		if not event:
-			raise ValueError("No event found")
-		else:
-			return event
 	
 	def write(self, file:io.TextIOBase):
 		"""Write the EDL to a given stream"""
@@ -309,12 +653,23 @@ class Edl:
 	@property
 	def tracks(self) -> list[Track]:
 		"""The tracks used in this EDL"""
-		return set(event.track for event in self.events)
+		tracks = set()
+		for e in self.events:
+			tracks = tracks.union(e.tracks)
+		return tracks
 	
 	@property
 	def events(self) -> list[Event]:
 		"""An EDL event"""
 		return self._events
+	
+	@property
+	def sources(self) -> set[SourceReel]:
+		"""A set of all sources in the EDL"""
+		sources = set()
+		for e in self.events:
+			sources = sources.union(e.sources)
+		return sources
 	
 	def __str__(self):
 		file_text = io.StringIO()
